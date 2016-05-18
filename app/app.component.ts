f9b0bd985c255fbf2a222e2b6f9c8cdb654f0407
dimavasheka <d1ma2000@ukr.net>
2016-05-18 17:43:01 +0300

    learn some feature

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,12 +2,22 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
 })
 export class AppComponent {
     title: string;
+    todos: string[];
 
     constructor () {
         this.title = 'Angular2';
+        this.todos = ['Изучить javascript', 'изучить ES6']
+    }
+
+    addTodo(input: HTMLInputElement) {
+        let title = input.value;
+        input.value = '';
+        console.log('Добавление задачи: ');
+        this.todos.push(input.value)
     }
 }
